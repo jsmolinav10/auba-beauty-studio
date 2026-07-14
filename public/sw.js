d0ad41fd10da4a02d9370e3709bd1fc40528a3a0
jsmolinav10 <jsmolinav10@users.noreply.github.com>
2026-07-13 19:20:09 -0500

    Bump service worker cache version to v8

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
  * Estrategia: Cache First para estáticos, Network First para API
  */
 
-const CACHE_NAME = 'auba-pwa-v7';
+const CACHE_NAME = 'auba-pwa-v8';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
